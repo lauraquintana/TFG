bdb7c11ca18557620121ee8ced834b91f29c841e
lauraquintana <75618444+lauraquintana@users.noreply.github.com>
2020-12-14 10:43:08 +0100

    update hemos puesto eventos javascript botones

---
+++ code.js
@@ -0,0 +1,16 @@
+//Funciones eventos
+function visibility1(){
+  document.getElementById("quiensoy").style.display="block"
+  document.getElementById("tfg2").style.display="none"
+  document.getElementById("program").style.display="none"
+}
+function visibility2(){
+  document.getElementById("quiensoy").style.display="none"
+  document.getElementById("tfg2").style.display="block"
+  document.getElementById("program").style.display="none"
+}
+function visibility3(){
+  document.getElementById("quiensoy").style.display="none"
+  document.getElementById("tfg2").style.display="none"
+  document.getElementById("program").style.display="block"
+}
